1a84f71848c4de2b1e6c281104b44bd1ff74ee27
bensboto <bensboto@gmail.com>
2023-02-28 20:08:41 -0700

    added basic inquirer function to accept prompts

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const {Shape, Circle, Triangle, Square} = require("./lib/shapes");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const circle = new Circle("BEN", "Green", "Blue");
-console.log(circle);
-console.log(circle.render());
+async function startInquirer() {
+    const {text, textColor, shapeColor, shape } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "text",
+          message: "What text would you like? Please enter 3 characters",
+        },
+        {
+          type: "input",
+          name: "textColor",
+          message: "What text color would you like?",
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "What shape would you like to make?",
+            choices: [
+              { name: "Circle", value: "circle" },
+              { name: "Triangle", value: "triangle" },
+              { name: "Square", value: "square" },
+            ],
+        },
+        {
+          type: "input",
+          name: "shapeColor",
+          message: "What color would you like the shape to be?",
+        },
+      ]);
+      console.log("Generated logo.svg!");
+}
+
+startInquirer();
