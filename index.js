b00f757d561d06fd1abb186eb7c2b9ccc6dacf7a
bensboto <bensboto@gmail.com>
2023-02-28 18:45:02 -0700

    added basic svg test files for 3 classes of shapes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const {Shape, Circle, Triangle, Square} = require("./lib/shapes");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const circle = new Circle("BEN", "Green", "Blue");
+console.log(circle);
+console.log(circle.render());
