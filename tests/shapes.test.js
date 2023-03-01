5dd8a53a21da4c4e4d34f4c41e7294f837d99663
bensboto <bensboto@gmail.com>
2023-03-01 12:47:16 -0700

    defined render methods and added tests for jest render methods

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -28,11 +28,29 @@ describe("Shape", () => {
 });
 
 describe("shapeColor", () => {
-    test("setColor method returns entered string", () => {
-      const shape = new Triangle();
-      shape.setColor("blue");
-      expect(shape.shapeColor).toEqual("blue");
+  test("setColor method returns entered string", () => {
+    const shape = new Triangle();
+    shape.setColor("blue");
+    expect(shape.shapeColor).toEqual("blue");
+  });
+});
+
+describe("renderTriangle", () => {
+  test("Render method returns correct svg string with color entered", () => {
+    const shape = new Triangle();
+    shape.setColor("blue");
+    expect(shape.render()).toEqual(
+      `<polygon points="150, 18 244, 182 56, 182" fill="blue"/>`
+    );
+  });
+});
+
+describe("render circle", () => {
+    test("Render method returns correct svg string with color entered", () => {
+      const shape = new Circle();
+      shape.setColor("green");
+      expect(shape.render()).toEqual(
+        `<circle cx="150" cy="100" r="80" fill="green"/>`
+      );
     });
   });
-
-
